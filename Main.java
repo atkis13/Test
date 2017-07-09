87bcb9823dac129504804b433667c912ebf5ac5c
atkis13 <kis_attila_1349@yahoo.com>
2017-07-09 11:26:19 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,21 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		Dog d1 = new Dog();
+		Dog d2 = new Dog();
+		Dog d3 = new Dog();
+		d1.setName("pista");
+		d2.setName("gyuri");
+		d3.setName("palko");
+		d1.setSize(90);
+		d2.setSize(40);
+		d3.setSize(10);
+		d1.bark(2);
+		d2.bark(5);
+		d3.bark(12);
+		
+
+	}
+
+}
